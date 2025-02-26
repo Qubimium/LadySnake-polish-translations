55e6a5af23ad17652e7084406f0a53230c2e455e
Qubimium <173802736+Qubimium@users.noreply.github.com>
2025-02-26 17:18:49 +0100

    deleted the Effective mod files hehehehehehehe
    also added Mariposa and Pick your poison translations

--- ideas.md
+++
@@ -1,7 +0,0 @@
-- Lava bubbles?
-- Shooting stars?
-- Ore shining / twinkling?
-- Campfire embers?
-- Sleeping foxes ZZZZZZZZZZZZZZZZZZs
-- Readd and overhaul glowworms
-- Better glow squid particles
--- src/client/java/org/ladysnake/effective/cosmetics/particle/contracts/FireflyParticleInitialData.java
+++
@@ -1,9 +0,0 @@
-package org.ladysnake.effective.cosmetics.particle.contracts;
-
-public class FireflyParticleInitialData {
-	public int color;
-
-	public FireflyParticleInitialData(int color) {
-		this.color = color;
-	}
-}
